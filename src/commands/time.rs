6d7bb91c597861ac1df89482b956c395520ce6d8
Thomas McNiven <hello@vevix.net>
2019-01-31 14:04:54 +1000

    support multiple timezones

--- src/commands/time.rs
+++ src/commands/time.rs
@@ -4,13 +4,16 @@ use select::predicate::Attr;
 use serenity::framework::standard::{Args, Command, CommandError as Error, CommandOptions};
 use serenity::model::channel::Message;
 use serenity::prelude::Context;
+use serenity::utils::MessageBuilder;
 use std::sync::Arc;
 
 pub struct Time;
 impl Command for Time {
     fn options(&self) -> Arc<CommandOptions> {
         Arc::new(CommandOptions {
-            desc: Some("Gets the current time in the timezone specified".into()),
+            desc: Some(
+                "Gets the current time in the timezone specified. Can be comma-delimited.".into(),
+            ),
             usage: Some("?time <timezone/city/country>".into()),
             min_args: Some(1),
             max_args: Some(1),
@@ -19,13 +22,22 @@ impl Command for Time {
     }
 
     fn execute(&self, _context: &mut Context, message: &Message, args: Args) -> Result<(), Error> {
-        let timezone = args.current().unwrap_or_default();
-        let endpoint = format!("https://time.is/{}", timezone);
-        let response = reqwest::get(&endpoint).unwrap();
-        let document = Document::from_read(response).unwrap();
-        let time = document.find(Attr("id", "twd")).next().unwrap().text();
-        let human_timezone = document.find(Attr("id", "msgdiv")).next().unwrap().text();
-        message.reply(&format!("{}: {}", human_timezone.trim(), time.trim()))?;
+        let timezones: Vec<&str> = args.rest().split(",").collect();
+        let mut times: Vec<String> = Vec::with_capacity(timezones.len());
+        for timezone in timezones {
+            let endpoint = format!("https://time.is/{}", timezone);
+            let response = reqwest::get(&endpoint).unwrap();
+            let document = Document::from_read(response).unwrap();
+            let time = document.find(Attr("id", "twd")).next().unwrap().text();
+            let human_timezone = document.find(Attr("id", "msgdiv")).next().unwrap().text();
+            times.push(format!("{}: {}", human_timezone.trim(), time.trim()));
+        }
+        let message_builder = MessageBuilder::new()
+            .mention(&message.author)
+            .push(" ")
+            .push_codeblock(times.join("\n"), None)
+            .build();
+        message.channel_id.say(&message_builder)?;
         Ok(())
     }
 }
