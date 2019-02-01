ef33a690833d9382a4956759f40a5c5e026102e9
Thomas McNiven <hello@vevix.net>
2019-02-01 23:19:30 +1000

    Use first_child on time node

--- src/commands/time.rs
+++ src/commands/time.rs
@@ -35,7 +35,7 @@ impl Command for Time {
             let response = reqwest::get(&endpoint).unwrap();
             let document = Document::from_read(response).unwrap();
             let time = document.find(Attr("id", "twd")).next().unwrap().text();
-            let human_timezone = document.find(Attr("id", "msgdiv")).next().unwrap().text();
+            let human_timezone = document.find(Attr("id", "msgdiv")).next().unwrap().first_child().unwrap().text();
             if human_timezone.trim().is_empty() {
                 message.reply(&format!(
                     "{} isn't valid... yikes... you really should learn your timezones.",
