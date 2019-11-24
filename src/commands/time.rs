50d823b9e7d81364d2af4e001d939b2d542f5907
Thomas McNiven <hello@vevix.net>
2019-11-24 21:26:10 +1100

    Update `time` command

--- src/commands/time.rs
+++ src/commands/time.rs
@@ -1,43 +1,32 @@
 use reqwest;
 use select::document::Document;
-use select::predicate::Attr;
+use select::predicate::Name;
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::utils::MessageBuilder;
 
+/// Melbourne, Australia; Kyiv, Ukraine
 #[command]
 fn time(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
-    let timezones: Vec<&str> = args.rest().split(',').collect();
+    let timezones: Vec<&str> = args.rest().split(';').collect();
     let mut times: Vec<String> = Vec::with_capacity(timezones.len());
     for timezone in timezones {
         let validated_timezone = timezone.replace(|c: char| !c.is_ascii(), "");
         if validated_timezone.trim().is_empty() {
             continue;
         }
+
         let endpoint = format!("https://time.is/{}", validated_timezone);
-        let response = reqwest::get(&endpoint).unwrap();
-        let document = Document::from_read(response).unwrap();
-        let time = document.find(Attr("id", "twd")).next().unwrap().text();
-        let human_timezone = document
-            .find(Attr("id", "msgdiv"))
-            .next()
-            .unwrap()
-            .first_child()
-            .unwrap()
-            .text();
-        if human_timezone.trim().is_empty() {
-            msg.reply(
-                &ctx,
-                &format!(
-                    "{} isn't valid... yikes... you really should learn your timezones.",
-                    validated_timezone
-                ),
-            )?;
-            continue;
+        let response = reqwest::get(&endpoint)?;
+
+        // If timezone is inacurate, the title will just be the local time for where this bot is running
+        let document = Document::from_read(response)?;
+        if let Some(time) = document.find(Name("time")).next() {
+            times.push(format!("{}: {}", timezone, time.text()));
         }
-        times.push(format!("{}: {}", human_timezone.trim(), time.trim()));
     }
+
     if !times.is_empty() {
         let message_builder = MessageBuilder::new()
             .mention(&msg.author)
@@ -46,5 +35,6 @@ fn time(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
             .build();
         msg.channel_id.say(&ctx, &message_builder)?;
     }
+
     Ok(())
 }
