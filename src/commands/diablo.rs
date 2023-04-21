e98063cb3032332d3d0fe7fed8bdc713e726f0f3
trmcnvn <hello@trmcnvn.dev>
2023-04-21 11:15:24 +1000

    add server slam

--- src/commands/diablo.rs
+++ src/commands/diablo.rs
@@ -7,10 +7,16 @@ use serenity::utils::MessageBuilder;
 
 #[command]
 async fn diablo(ctx: &Context, msg: &Message) -> CommandResult {
+    // Diablo 4 stress test
+    let st_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 5, 12).and_hms(19, 0, 0), Utc);
     // Diablo 4 launch date
     let rl_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 6, 2).and_hms(16, 0, 0), Utc);
     // format message
     let message = MessageBuilder::new()
+        .push_line(format!(
+            "**Server Slam**: {}",
+            HumanTime::from(st_dt).to_text_en(Accuracy::Precise, Tense::Present)
+        ))
         .push_line(format!(
             "**Release**: {}",
             HumanTime::from(rl_dt).to_text_en(Accuracy::Precise, Tense::Present)
