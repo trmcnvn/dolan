38ffa35eab76b48eebc056e230a7347db7e7e43e
trmcnvn <hello@trmcnvn.dev>
2023-05-26 12:26:54 +1000

    Update dates and preload

--- src/commands/diablo.rs
+++ src/commands/diablo.rs
@@ -7,19 +7,19 @@ use serenity::utils::MessageBuilder;
 
 #[command]
 async fn diablo(ctx: &Context, msg: &Message) -> CommandResult {
-    // Diablo 4 stress test
-    let st_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 5, 12).and_hms(19, 0, 0), Utc);
+    // Diablo 4 preload date
+    let pl_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 5, 30).and_hms(23, 0, 0), Utc);
     // Diablo 4 launch date
-    let rl_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 6, 2).and_hms(16, 0, 0), Utc);
-    // format message
+    let rl_dt = DateTime::<Utc>::from_utc(NaiveDate::from_ymd(2023, 6, 2).and_hms(23, 0, 0), Utc);
+    // format messag
     let message = MessageBuilder::new()
         .push_line(format!(
-            "**Server Slam**: {}",
-            HumanTime::from(st_dt).to_text_en(Accuracy::Precise, Tense::Present)
+            "**Preload**: {}",
+            HumanTime::from(pl_dt).to_text_en(Accuracy::Precise, Tense::Future)
         ))
         .push_line(format!(
             "**Release**: {}",
-            HumanTime::from(rl_dt).to_text_en(Accuracy::Precise, Tense::Present)
+            HumanTime::from(rl_dt).to_text_en(Accuracy::Precise, Tense::Future)
         ))
         .build();
     msg.channel_id.say(&ctx, message).await?;
